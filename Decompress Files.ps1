4592b41b2518dfd3c09a1f4a4286ce4905ae7674
Mohamed Amine Boulaayoun <33926500+mohamedamine724@users.noreply.github.com>
2019-01-09 12:02:25 +0100

    Powershell script to decompress files

---
+++ Decompress Files.ps1
@@ -0,0 +1,25 @@
+﻿# Primer de tot definim els tipus de compressió..
+$tipus = @("*.zip", "*.rar")
+
+#Realitzem el bucle per provar els diferents tipos (en aquest cas farà 2 voltes perquè en la array tenim 2 tipus)
+foreach ($tipo in $tipus)
+{
+    #Aquesta línia busca els fitxers comprimits.
+    $Zips = Get-ChildItem -filter "$tipo" -path "C:\Users\DAP Informàtica\Desktop\hola" -Recurse
+
+    #Definim la ruta de destí.
+    $Destination = 'C:\Users\DAP Informàtica\Desktop\hola'
+
+    #Definim la ruta del programa WinRAR.
+    $WinRar = "C:\Program Files\WinRAR\winrar.exe"
+
+    #Aquest procés descomprimeix els fitxers.
+    foreach ($zip in $Zips)
+    {
+        &$Winrar x $zip.FullName $Destination
+        Get-Process winrar | Wait-Process
+    }
+  
+
+}
+
